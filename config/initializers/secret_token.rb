0abab52885fe445956902db48f9297a2fa35d0d7
chrisgay <chris.gay@cmg2.co>
2012-10-01 21:27:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoiceConcierge::Application.config.secret_token = '510a2bc239cd8aacee906fa98771d4ccecd33a89d4d8bc95a74b4711690e8a5eeb8c946adcf5194c45177056e9d5c16bcf8ca5822b5748cca66f5f5781f4bb7c'
